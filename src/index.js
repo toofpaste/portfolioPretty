4e25c0eb5084679c3fb5d971b95a232d1c9d251a
Michael Larragueta <mjlarragueta@gmail.com>
2020-02-19 15:43:36 -0800

    finish splash page

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './styles.css';
+import bg1 from './img/pietro-de-grandi-y5vLKnZr6Zg-unsplash.jpg';
+import bg2 from './img/pietro-de-grandi-y5vLKnZr6Zg-unsplash2.png';
+import git from './img/github.png';
+import linkd from './img/linkedin.png';
+const ele1 = document.getElementById('bgImage1');
+const ele2 = document.getElementById('bgImage2');
+ele1.src = bg1;
+ele2.src = bg2;
+const ele3 = document.getElementById("linkedin");
+const ele4 = document.getElementById('github');
+ele3.src = linkd;
+ele4.src = git;
+$(document).ready(function(){
+
+});
